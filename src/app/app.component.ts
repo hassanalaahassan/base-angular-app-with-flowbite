825c106f571966db7dce6bf28467f12edf13c461
HASSAN <hassanalaa6060@gmail.com>
2026-02-09 17:03:34 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, OnInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { filter, Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { SidebarComponent } from "./shared/components/side-bar/side-bar.component";
 import { TopBarComponent } from "./shared/components/top-bar/top-bar.component";
 import { LayoutStore } from './core/Services/layout.service';
@@ -10,7 +11,8 @@ import { LayoutStore } from './core/Services/layout.service';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, SidebarComponent, TopBarComponent],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
   // Inject stores
